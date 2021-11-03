aafeab0979599bc038fda1389a081b4d6f244f02
z20ist24 <z20ist24@Z00P1-05>
2021-11-03 08:49:52 +0100

    checks if a string contains two consecutive matching letters

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            {
+                for (int i = 0; i < str.Length -1; i++)
+                {
+                    char c = str[i];
+                 
+                    if (c == str[i + 1])
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
         }
     }
 }
